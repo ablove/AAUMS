ef55d4e2caeab7790e550f9326ac3da0776a8d21
Ashenafidejene <ashenafidejene75@gmail.com>
2024-10-18 22:25:19 +0300

    create a react app for front end

---
+++ aaums-frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         this is the coding part 
+      </header>
+    </div>
+  );
+}
+
+export default App;
